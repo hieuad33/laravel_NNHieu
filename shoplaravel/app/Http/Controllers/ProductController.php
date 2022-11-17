e09b6ef641eadbd17f5b390b224dadf58ca5653f
hieuad33 <39626889+hieuad33@users.noreply.github.com>
2022-11-17 23:26:57 +0700

    1
    1

--- shoplaravel/app/Http/Controllers/ProductController.php
+++ shoplaravel/app/Http/Controllers/ProductController.php
@@ -152,7 +152,7 @@ class ProductController extends Controller
         ->join('tbl_category_product','tbl_category_product.category_id','=','tbl_product.category_id')
         ->join('tbl_brand','tbl_brand.brand_id','=','tbl_product.brand_id')
         ->where('tbl_product.product_id',$product_id)->get();
-
+        
         foreach($details_product as $key => $value){
             $category_id = $value->category_id;
             $product_slug = $value->product_slug;
@@ -168,7 +168,7 @@ class ProductController extends Controller
         $related_product = DB::table('tbl_product')
         ->join('tbl_category_product','tbl_category_product.category_id','=','tbl_product.category_id')
         ->join('tbl_brand','tbl_brand.brand_id','=','tbl_product.brand_id')
-        ->where('tbl_category_product.category_id',$category_id)->whereNotIn('tbl_product.product_slug',[$product_slug])->get();
+        ->where('tbl_category_product.category_id',$details_product->category_id)->whereNotIn('tbl_product.product_slug',[$product_slug])->get();
 
         $manager_product  = view('pages.sanpham.show_details')->with('details_product',$details_product)->with('category',$cate_product)->with('brand',$brand_product)->with('relate',$related_product);
 
